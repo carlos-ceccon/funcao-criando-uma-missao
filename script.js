91f8cc6ade8ab4b5e730b771bd22132f3039ead9
carlos-ceccon <140418813+carlos-ceccon@users.noreply.github.com>
2025-06-23 08:41:40 -0300

    modifiquei o codigo

--- script.js
+++ script.js
@@ -53,31 +53,18 @@ function mostraPergunta() {
   caixaPerguntas.textContent = perguntaAtual.enunciado;
   mostraAlternativas();
 }
-function mostraAlternativas() {
-  for (const alternativa of perguntaAtual.alternativas) {
-    const botaoAlternativas = document.createElement("button");
-    botaoAlternativas.textContent = alternativa;
-    caixaAlternativas.appendChild(botaoAlternativas);
-  }
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
-
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
 mostraPergunta();
-botao.addEventListener("click", function () {
-  respostaSelecionada(opcao);
-});
-botao.addEventListener("click", () => respostaSelecionada(opcao));
-function respostaSelecionada(opcaoSelecionada) {
-  atual++;
-  mostraPergunta();
-}
-function respostaSelecionada(opcaoSelecionada) {
-  const afirmacoes = opcaoSelecionada.afirmacoes;
-  atual++;
-  mostraPergunta();
-}
-function respostaSelecionada(opcaoSelecionada) {
-  const afirmacoes = opcaoSelecionada.afirmacoes;
-  historiaFinal += afirmacoes + " ";
-  atual++;
-  mostraPergunta();
-}
